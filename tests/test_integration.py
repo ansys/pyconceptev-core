e06d882212862ea94087a6c65a6fad400de54e9c
Kathy Pippert <kathy.pippert@ansys.com>
2024-05-06 14:11:01 -0400

    Fix bad possessive

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -22,7 +22,7 @@
 
 """Integration tests.
 
-These integration tests verify that the deployed API's match the CLI client.
+These integration tests verify that the deployed APIs match the CLI client.
 """
 
 import datetime
